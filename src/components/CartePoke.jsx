65568ba47b2ae3dac1eac4d8168d8cf769b00dbb
kottor <gardien243@gmail.com>
2025-06-02 19:37:03 +0200

    fin du style

--- src/components/CartePoke.jsx
+++ src/components/CartePoke.jsx
@@ -3,7 +3,7 @@ export default function CartePoke({el}){
         <div className={`pokeCarte ${el.apiTypes[1]?el.apiTypes[1].name:el.apiTypes[0].name}`}>
             <div className="pokeCarte_head">
                 <h4>{el.name}</h4>
-                <h5>{el.pokedexId}</h5>
+                <h5 className="pokeCarte_id">{el.pokedexId}</h5>
             </div>
             <div className="pokeCarte_body">
                 <img src={el.sprite} alt={el.name} />
